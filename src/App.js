ade19e9f16228f8177d5a83da1db276eacc300f6
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-14 06:51:45 -0400

    add delete functionality (window.confirm deletion)

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ React refs and useRef
 
 */
 
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ]);
+  
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+
+      <TodoList todos={todos} setTodos={setTodos} />
+      <AddToDo setTodos={setTodos} />
+    </div>
+  );
+}
+
 function TodoList({ todos, setTodos }) {
   function handleToggleTodo(todo) {
     // if t.id === todo.id => changes done value to opposite
@@ -26,6 +44,11 @@ function TodoList({ todos, setTodos }) {
     })
     setTodos(updatedTodos)
   }
+  
+  if (!todos.length) {
+    return <p>No todos left!</p>;
+  }
+  
   return (
     <ul>
       {todos.map((todo) => (
@@ -37,10 +60,38 @@ function TodoList({ todos, setTodos }) {
           key={todo.id}
         >
           {todo.text}
+          {/* pass todo data down as a prop to DeleteTodo */}
+          <DeleteTodo todo={todo} setTodos={setTodos} />
         </li>
       ))}
     </ul>
   )
+}
+
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
+
+  return (
+    <span
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer"
+      }}
+    >
+      x
+    </span>
+  );
 }
 
 function AddToDo({ setTodos }) {
@@ -65,21 +116,4 @@ function AddToDo({ setTodos }) {
       <button type="submit">Submit</button>
     </form>
   )
-}
-
-export default function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "Wash dishes", done: false },
-    { id: 2, text: "Do laundry", done: false },
-    { id: 3, text: "Take shower", done: false }
-  ]);
-
-  return (
-    <div className="App">
-      <h1>Todo List</h1>
-
-      <TodoList todos={todos} setTodos={setTodos} />
-      <AddToDo setTodos={setTodos} />
-    </div>
-  );
 }
