51166cb1cc89a5b7422242c84c3c8a5a72573ba7
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-14 06:38:32 -0400

    AddToDo component (form)

--- src/App.js
+++ src/App.js
@@ -17,6 +17,24 @@ function TodoList({ todos }) {
   )
 }
 
+function AddToDo() {
+  function handleAddTodo(event) {
+    event.preventDefault()
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false
+    };
+  }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input placeholder="Add todo" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
 export default function App() {
   const todos = [
     { id: 1, text: "Wash dishes", done: false },
@@ -29,6 +47,7 @@ export default function App() {
       <h1>Todo List</h1>
 
       <TodoList />
+      <AddToDo />
     </div>
   );
 }
