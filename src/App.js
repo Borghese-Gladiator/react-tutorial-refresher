269dd8f96e5f327ed0cdbd0c5c556e524a4afa0b
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-14 06:33:32 -0400

    render list of todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
 
+/*
+NOTES:
+React Keys: If we were to update a todo's text or done value, the key is what tells React which todo item needs to be updated.
+*/
+
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {
+        todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))
+      }
+    </ul>
+  )
+}
+
 export default function App() {
+  const todos = [
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Todo List</h1>
+
+      <TodoList />
     </div>
   );
 }
